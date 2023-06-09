d9300d9f31766ed409654c82f853589ecc570771
Pedro Melo <pedromelo1800@gmail.com>
2023-06-09 15:58:53 -0300

    Updates

---
+++ js/about.dd3497de.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunksterling_frontend"]=self["webpackChunksterling_frontend"]||[]).push([[443],{4007:function(n,t,e){e.r(t),e.d(t,{default:function(){return l}});var r=e(3396);const s={class:"about"},u=(0,r._)("h1",null,"This is an about page",-1),a=[u];function i(n,t){return(0,r.wg)(),(0,r.iD)("div",s,a)}var c=e(89);const o={},f=(0,c.Z)(o,[["render",i]]);var l=f}}]);
+//# sourceMappingURL=about.dd3497de.js.map
